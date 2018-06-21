199560216051a739e4d326f920be39279269633f
nard <gim.kobayashi@gmail.com>
2018-06-21 16:52:08 +0900

    update

--- control-k8s/stress-testing.py
+++ control-k8s/stress-testing.py
@@ -6,16 +6,34 @@ import json
 from pathlib import Path
 
 from concurrent.futures import ProcessPoolExecutor as PPE
+import time
 
-paths = [path for path in Path('../objs').glob('*') ]
+import statistics
+import random
+
+paths = sorted([path for path in Path('../objs').glob('*') ])[:1000]
+
 
 def pmap(path):
-  obj = json.load( path.open() )
-  texts = obj['text']
-  payload = json.dumps( {'texts': texts} )
-  r = requests.post("http://35.232.164.217", data=payload)
-  print(r.text, texts)
-  #print(obj)
+  try:
+    obj = json.load( path.open() )
+    texts = obj['text']
+    payload = json.dumps( {'texts': texts} )
+    r = requests.post("http://35.232.164.217", data=payload, timeout=10)
+    print(r.text, texts)
+    if random.random() < 0.05:
+      print(r.text, texts)
+    #print(obj)
+  except Exception as ex:
+    print(ex)
 
-with PPE(max_workers=32) as exe:
-  exe.map(pmap, paths)
+elapseds = []
+for i in range(6):
+  start = time.time()
+  with PPE(max_workers=10) as exe:
+    exe.map(pmap, paths)
+
+  print(f'elapsed time {time.time() - start}')
+  elapseds.append( time.time() - start )
+
+print('statistics', statistics.mean(elapseds) )
